c23e40b42f2a06404fb212b97127564f7b0b2b73
Wylie Fisher <wylie@dukeofcheese.com>
2017-08-01 12:04:14 -0400

    capitals

--- playerMove.js
+++ playerMove.js
@@ -3,7 +3,6 @@
 
 events.blockBreak( function( event ) {
   echo( event.player, event.player.name + ', you broke a block!');
-  var locX = event.block.location;
-  var locY = event.block.location.y;
-  console.log(locX);
+  var location = event.block.Location;
+  console.log(location);
 });
