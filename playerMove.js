a6e67883dacd4c81aa6489e0428e781f4a3e0e6c
Wylie Fisher <wylie@dukeofcheese.com>
2017-08-01 11:11:56 -0400

    updating this console.log

--- playerMove.js
+++ playerMove.js
@@ -4,5 +4,5 @@
 events.blockBreak( function( event ) {
   echo( event.player, event.player.name + ', you broke a block!');
   var location = event.block.location;
-  console.log('this location: ', location);
+  console.log('This location is: ', location);
 });
